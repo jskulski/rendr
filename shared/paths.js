8b30a621059f9f1bce9df69e46ca010a695f4d79
jskulski <jskulski@trulia.com>
2013-11-13 00:34:47 -0800

    mend

--- shared/paths.js
+++ shared/paths.js
@@ -1,32 +1,66 @@
+/**
+ * Paths determines where components of an app live. It is globally accessible as:
+ *
+ * `rendr.paths`
+ *
+ * and maybe over written to suit your needs. See 05_modules in the example directory.
+ * The base layout of an app follows the examples:
+ *
+ * app/
+ *     app.js
+ *     routes.js
+ *     controllers/
+ *       controller_one.js
+ *       controller_two.js
+ *     models/
+ *     collections/
+ *     views/
+ *       some_view.js
+ *       controller/action.js
+ *     controllers/
+ */
 var paths = {
 
   entryPath: '',
 
   routesPath: 'app/routes',
-  controllerPath:'app/controllers',
-  viewsPath: 'app/views',
 
-  getPath: function(thing) {
+  modelsPath: 'app/models',
+  collectionsPath: 'app/collections',
+  viewsPath: 'app/views',
+  controllersPath:'app/controllers',
+
+  getPath: function getPath(thing) {
     thing = thing || '';
     var path = this.entryPath + thing;
     return path;
   },
 
-  getRoutesPath: function() {
+  getRoutesPath: function getRoutesPath() {
     var path = this.entryPath + this.routesPath;
     return path;
   },
 
-  getViewPath: function(viewName) {
+  getModelPath: function getModelPath(modelName) {
+    var path = [this.entryPath, this.modelsPath, modelName].join('/');
+    return path;
+  },
+
+  getCollectionPath: function getCollectionPath(collectionName) {
+    var path = [this.entryPath, this.collectionsPath, collectionName].join('/');
+    return path;
+  },
+
+  getViewPath: function getViewPath(viewName) {
     var path = [this.entryPath, this.viewsPath, viewName].join('/');
     return path;
   },
 
-  getControllerPath: function(controllerName) {
-    var path = [this.entryPath, this.controllerPath, controllerName + '_controller'].join('/');
+  getControllerPath: function getControllerPath(controllerName) {
+    var path = [this.entryPath, this.controllersPath, controllerName + '_controller'].join('/');
     return path;
   }
 
 };
 
-module.exports = paths
+module.exports = paths;
